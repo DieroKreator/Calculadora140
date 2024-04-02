083a4912532eecc84293a7d4e7358214abb66955
DieroKreator <qtechwired@gmail.com>
2024-04-02 00:44:13 -0300

    Added 3 more tests

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -21,3 +21,33 @@ def test_somar_dois_numeros():
 
     # Assert / Valida
     assert resultado_esperado == resultado_obtido
+
+
+def test_subtrair_dois_numeros():
+    num1 = 5
+    num2 = 7
+    resultado_esperado = -2
+
+    resultado_obtido = subtrair_dois_numeros(num1, num2)
+
+    assert resultado_esperado == resultado_obtido
+
+
+def test_multiplicar_dois_numeros():
+    num1 = 5
+    num2 = 7
+    resultado_esperado = 35
+
+    resultado_obtido = multiplicar_dois_numeros(num1, num2)
+
+    assert resultado_esperado == resultado_obtido
+
+
+def test_dividir_dois_numeros():
+    num1 = 5
+    num2 = 2
+    resultado_esperado = 2.5
+
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
+    assert resultado_esperado == resultado_obtido
--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -5,7 +5,7 @@ def subtrair_dois_numeros(num1, num2):
     return num1 - num2
 
 def  multiplicar_dois_numeros(num1, num2):
-    return num1 + num2
+    return num1 * num2
 
 def dividir_dois_numeros(num1, num2):
     try:
