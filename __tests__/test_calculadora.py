43822ea3fa077766db0bbb94b04d389c3ef9f851
DieroKreator <qtechwired@gmail.com>
2024-04-07 15:28:16 -0300

    Added a negative test for division function

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -24,12 +24,15 @@ def test_somar_dois_numeros():
 
 
 def test_subtrair_dois_numeros():
+    # Configura
     num1 = 5
     num2 = 7
     resultado_esperado = -2
 
+    # Executa
     resultado_obtido = subtrair_dois_numeros(num1, num2)
 
+    # Valida
     assert resultado_esperado == resultado_obtido
 
 
@@ -47,6 +50,15 @@ def test_dividir_dois_numeros():
     num1 = 5
     num2 = 2
     resultado_esperado = 2.5
+
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
+    assert resultado_esperado == resultado_obtido
+
+def test_dividir_por_zero():
+    num1 = 5
+    num2 = 0
+    resultado_esperado = 'Erro: Não é possível dividir por zero'
 
     resultado_obtido = dividir_dois_numeros(num1, num2)
 
